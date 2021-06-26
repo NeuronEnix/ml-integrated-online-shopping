529b5e0dbc0094c81dcc5f9ec072c274df3c97ca
NeuronEnix <kaushikrb909@gmail.com>
2021-06-26 16:45:11 +0530

    Added: name for offer

--- api/shop/controllers/offer.js.js
+++ api/shop/controllers/offer.js.js
@@ -1,4 +1,6 @@
 const ShopModel = require('../model');
+const mongoose = require( "mongoose" );
+const ItemModel = require( "../../item/model" );
 
 const { resOk, resErr, resErrType } = require('../../../handlers/responseHandler');
 
@@ -24,11 +26,20 @@ module.exports.listOffer = async ( req, res, next ) => {
     try {
         
         const shopDoc = await ShopModel.findById( req.user.shopID );
-
         if ( !shopDoc )
             return resErr( res, resErrType.resNotFound, { infoToClient: "Shop Not Found" } );
+        const onSaleItems = [];
 
-        return resOk( res, shopDoc.onSale );
+        for ( item of shopDoc.onSale ){
+            const curItem = await ItemModel.findOne( { _id: item.itemID } );
+            onSaleItems.push({
+                itemID: item.itemID, offer: item.offer,
+                price: curItem.subDetail[0].price,
+                name: curItem.name,
+            })            
+        }
+        
+        return resOk( res, onSaleItems );
         
     } catch ( err ) {
         return next( { _AT: __filename, err } );
