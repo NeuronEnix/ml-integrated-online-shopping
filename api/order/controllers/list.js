43515af127290e4b1188bf30dcd73c46dbced5ea
NeuronEnix <kaushikrb909@gmail.com>
2021-07-01 13:21:18 +0530

    Added: orderID for order view

--- api/order/controllers/list.js
+++ api/order/controllers/list.js
@@ -9,13 +9,15 @@ module.exports = async ( req, res, next ) => {
     try {
         const { userID, shopID } = req.user; 
 
-        const orderDoc = await OrderModel.find( { userID, shopID }, { _id:0, __v:0} ).lean();
+        const orderDoc = await OrderModel.find( { userID, shopID }, { __v:0 } ).lean();
         
         if ( !orderDoc )
             return resErr( res, resErrType.resNotFound, { infoToClient: "Invalid Order" } );
 
         for ( order of orderDoc ) {
             order.itemObj = await ItemModel.findById( order.itemID, { _id:0, __v:0 } ).lean();
+            order.orderID = order._id;
+            delete order._id;
         }
 
         return resOk( res, orderDoc );
