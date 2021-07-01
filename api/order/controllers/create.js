f97897bc1b543f0d2864881ace73242e27fe483a
NeuronEnix <kaushikrb909@gmail.com>
2021-07-01 12:57:50 +0530

    Added: payment for order create

--- api/order/controllers/create.js
+++ api/order/controllers/create.js
@@ -3,16 +3,19 @@ const UserModel = require( "../../user/model" );
 const ShopModel = require( "../../shop/model" );
 const ItemModel = require( "../../item/model" );
 
+const { stripeCheckout } = require('../../../services/stripeCheckout' );
 const { resOk, resErr, resErrType } = require('../../../handlers/responseHandler');
 
 module.exports = async ( req, res, next ) => {
     try {
         const { userID, shopID } = req.user; 
 
-        
-
-        const cart = ( await UserModel.findById( userID ).lean() ).cart;
+        const userDoc = await UserModel.findById( userID ).lean();
+        const cart = userDoc.cart;
         const shopDoc = await ShopModel.findById( shopID ).lean();
+
+        let totPrice = 0;
+
         for ( item of cart ) {
             item.shopID = shopID;
             item.userID = userID;
@@ -20,13 +23,31 @@ module.exports = async ( req, res, next ) => {
             item.price = itemDoc.subDetail.find( sub => String( item.subID ) == String( sub._id ) ).price;
             item.offer = shopDoc.onSale.find( offerItem => String( offerItem.itemID ) == String( item.itemID ) )?.offer || 0;
 
+            totPrice += parseInt( item.price );
+
+        }
+
+        // Payment
+        const stripeToken = req.body;
+        const userMetadata = { userID: req.user.userID };
+        const stripeResult = await stripeCheckout( stripeToken, totPrice, userDoc.email, "1234567890", userDoc.name, userMetadata );
+
+        if ( stripeResult.status !== "succeeded" ) 
+            return resErr( res, resErrType.stripeError, { 
+                infoToClient: "Payment Failed: " + stripeResult.msg,
+                infoToServer: stripeResult
+            })
+
+        for ( item of cart ) {
             const orderDoc = await OrderModel();
             Object.assign( orderDoc, item );
             await orderDoc.save();
         }
-        return resOk( res );
+
+        return resOk( res, stripeResult );
 
     } catch ( err ) {
         return next( { _AT: __filename, err } );
     }
 }
+
