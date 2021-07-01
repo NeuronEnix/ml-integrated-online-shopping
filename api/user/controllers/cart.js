79dfc8d560edcce4a94d800adc2f972584a7a46e
NeuronEnix <kaushikrb909@gmail.com>
2021-07-01 10:52:45 +0530

    Added: offer data to cart view

--- api/user/controllers/cart.js
+++ api/user/controllers/cart.js
@@ -1,5 +1,7 @@
 const UserModel = require('../model');
 const ItemModel = require( "../../item/model" );
+const ShopModel = require( "../../shop/model" );
+const mongoose = require( "mongoose" );
 
 const { resOk, resErr, resErrType } = require('../../../handlers/responseHandler');
 
@@ -28,10 +30,13 @@ module.exports.view = async ( req, res, next ) => {
 
         if ( !userDoc ) return resErr( res, resErrType.unAuthorized, { infoToClient: "Invalid User" } );
         cartList = [];
+        const onSale = (await ShopModel.findById( shopID ).lean()).onSale;
+        
         for ( cart of userDoc.cart ) {
             cartList.push({
                 itemID: cart.itemID,
                 subID: cart.subID,
+                offer: onSale.find( eachOffer => String( eachOffer.itemID ) == String( cart.itemID )  ),
                 qty: cart.qty,
                 itemObj: (await ItemModel.findById( cart.itemID, { _id:0, __v:0} )) ,
             }) 
