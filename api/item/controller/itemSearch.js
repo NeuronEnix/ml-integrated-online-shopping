7204b3ff7ada3155cc77c4b0b931352c1eea8019
Sasuke-003 <56964273+Sasuke-003@users.noreply.github.com>
2021-07-13 12:40:12 +0530

    NIce

--- api/item/controller/itemSearch.js
+++ api/item/controller/itemSearch.js
@@ -1,58 +1,56 @@
-const ItemModel = require( "../model" );
-const ShopModel = require( "../../shop/model" );
-const { resOk, resErr, resErrType } = require('../../../handlers/responseHandler');
+const ItemModel = require("../model");
+const ShopModel = require("../../shop/model");
+const { resOk, resErr, resErrType } = require("../../../handlers/responseHandler");
 const mongoose = require("mongoose");
 
-
-module.exports = async (req, res, next ) => {
-
+module.exports = async (req, res, next) => {
     try {
-
         const { itemName } = req.query;
         const { shopID } = req.user;
-        const regExItemName = ".*" + itemName.split("").join( ".*" ) + ".*" ; 
+        const regExItemName = ".*" + itemName.split("").join(".*") + ".*";
 
-        console.log( regExItemName );
+        console.log(regExItemName);
 
         const itemList = await ItemModel.aggregate([
-            { $match : { shopID: mongoose.Types.ObjectId( shopID ), name : new RegExp( regExItemName ),  } },
-            { $sort:{ name:1 } },
-            { $project: { 
-                name:1, itemID: "$_id", img:1,
-                rating: { $divide: [ "$rateSum", { $cond: [ { $eq: [ "$rateCount", 0 ] }, 1, "$rateCount" ] } ] },
-                _id:0,
-            } },
+            { $match: { shopID: mongoose.Types.ObjectId(shopID), name: new RegExp(regExItemName) } },
+            { $sort: { name: 1 } },
+            {
+                $project: {
+                    name: 1,
+                    itemID: "$_id",
+                    img: 1,
+                    rating: { $divide: ["$rateSum", { $cond: [{ $eq: ["$rateCount", 0] }, 1, "$rateCount"] }] },
+                    _id: 0,
+                },
+            },
         ]);
 
         const itemListByCategory = await ItemModel.aggregate([
-            { $match : { shopID: mongoose.Types.ObjectId( shopID ), category : new RegExp( regExItemName ),  } },
-            { $sort:{ name:1 } },
-            { $project: { 
-                name:1, itemID: "$_id", img:1,
-                rate: { $divide: [ "$rateSum", { $cond: [ { $eq: [ "$rateCount", 0 ] }, 1, "$rateCount" ] } ] },
-                _id:0,
-            } },
+            { $match: { shopID: mongoose.Types.ObjectId(shopID), category: new RegExp(regExItemName) } },
+            { $sort: { name: 1 } },
+            {
+                $project: {
+                    name: 1,
+                    itemID: "$_id",
+                    img: 1,
+                    rate: { $divide: ["$rateSum", { $cond: [{ $eq: ["$rateCount", 0] }, 1, "$rateCount"] }] },
+                    _id: 0,
+                },
+            },
         ]);
 
-        for ( eachItem of itemListByCategory )
-            if ( !itemList.find( item => String(eachItem.itemID) == String( item.itemID ) ) )
-                itemList.push( eachItem );
+        for (eachItem of itemListByCategory) if (!itemList.find((item) => String(eachItem.itemID) == String(item.itemID))) itemList.push(eachItem);
 
+        const onSale = (await ShopModel.findById(shopID).lean()).onSale;
 
-        const onSale = (await ShopModel.findById( shopID ).lean()).onSale;
-
-        for ( item of itemList ) {
-            item.price = (await ItemModel.findById( item.itemID )).subDetail[0].price;
-            item.itemObj = await ItemModel.findById( item.itemID , { rateSum:0, rateCount:0, __v:0 } );
-            item.offer = onSale.find( eachOffer => String( eachOffer.itemID ) == String( itemID )  )?.offer || 0;
+        for (item of itemList) {
+            item.price = (await ItemModel.findById(item.itemID)).subDetail[0].price;
+            item.itemObj = await ItemModel.findById(item.itemID, { rateSum: 0, rateCount: 0, __v: 0 });
+            item.offer = onSale.find((eachOffer) => String(eachOffer.itemID) == String(item.itemID))?.offer || 0;
         }
-        
-        return resOk( res, { itemList, onSale: (await ShopModel.findById( shopID )).onSale } ) ;
 
-    } catch( err ) {
-
-        return next( { _AT: __filename, err } );
-
+        return resOk(res, { itemList, onSale: (await ShopModel.findById(shopID)).onSale });
+    } catch (err) {
+        return next({ _AT: __filename, err });
     }
-
 };
