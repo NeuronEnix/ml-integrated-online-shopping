e45f0b15398e90fd69c837bdeb4762e663c98ba8
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2021-07-14 11:15:05 +0530

    Added: shopID to signUp API

--- api/user/controllers/signIn.js
+++ api/user/controllers/signIn.js
@@ -18,7 +18,7 @@ module.exports = async ( req, res, next ) => {
         refTok.addToCookie( res, rTok );
         const accTokPayload = { userID: userDoc._id, typ: userDoc.typ, shopID: userDoc.shopID, tokID: refTok.decode( rTok ).tokID }
 
-        return resOk( res, { accTok: accTok.sign( accTokPayload ), typ: userDoc.typ } );
+        return resOk( res, { accTok: accTok.sign( accTokPayload ), typ: userDoc.typ, shopID: userDoc.shopID } );
         
     } catch ( err ) {
         return next( { _AT: __filename, err } );
