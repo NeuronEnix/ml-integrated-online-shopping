72c5d4965dff2f64965ebb3d936b3a0f358b08ca
Kaushik R Bangera <50287102+NeuronEnix@users.noreply.github.com>
2021-07-14 10:31:36 +0530

    Added: color to 'shop-detail' API

--- api/shop/model.js
+++ api/shop/model.js
@@ -7,9 +7,9 @@ const shopSchema = new mongoose.Schema ({
     // Data
     name: { type: String, uppercase: true },
     address: { type: String, uppercase: true },
-    pinCode: { type: String, uppercase: true },
     onSale: { type: [ { _id: false, itemID: ObjectID, offer: Number } ] , default: [] },
     banner: { type: String, default: 'none' },
+    color: { type: {} , default: {} },
     
     // Meta
     userID: { type: ObjectID, required: true },
--- api/shop/validation.js
+++ api/shop/validation.js
@@ -4,7 +4,8 @@ const { validate } = require('../../handlers/validationHandler');
 module.exports.schema = schema = {
     name: Joi.string().label("Shop Name").trim().min(1).max(40).uppercase().pattern( new RegExp( '^[A-Z ]+$' ) ),
     address: Joi.string().label("Address").trim().min(1).max(40).uppercase().pattern( new RegExp( '^[A-Z a-z 0-9]+$' ) ),
-    pinCode: Joi.string().label("Pin Code").trim().min(1).max(6).uppercase().pattern( new RegExp( '[0-9]' ) ),
+    // pinCode: Joi.string().label("Pin Code").trim().min(1).max(6).uppercase().pattern( new RegExp( '[0-9]' ) ),
+    color: Joi.any(),
     userID: Joi.string().label("userID").trim().min(24).max(24).alphanum(),
 }
 
@@ -12,7 +13,8 @@ module.exports.schema = schema = {
 const updateShopSchema = Joi.object({
     name: schema.name.required(),
     address: schema.address.required(),
-    pinCode: schema.pinCode.required(),
+    // pinCode: schema.pinCode.required(),
+    color: schema.color.required(),
 });
 
 module.exports.updateShop = ( req, res, next ) => validate( req, res, next, updateShopSchema, 'body' );
